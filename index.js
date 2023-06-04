95f1980dfdb1f28ce1273de47c43b49188de2917
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-06-04 18:24:46 +0600

    complete milstone12

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ async function run() {
     //create payment intent
     app.post("/create-payment-intent", verifyJwt, async (req, res) => {
       const { price } = req.body;
-      const amount = price*100;
+      const amount = parseInt(price*100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
@@ -206,6 +206,68 @@ async function run() {
       const deleteResult = await cartCollection.deleteMany(query)
 
       res.send({insertResult, deleteResult});
+    })
+
+    app.get('/admin-state',verifyJwt, verifyJwt, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment)=> payment.price + sum , 0)
+      res.send({
+        users,
+        products,
+        orders,
+        revenue
+      })
+    })
+
+     /**
+     * ---------------
+     * BANGLA SYSTEM(second best solution)
+     * ---------------
+     * 1. load all payments
+     * 2. for each payment, get the menuItems array
+     * 3. for each item in the menuItems array get the menuItem from the menu collection
+     * 4. put them in an array: allOrderedItems
+     * 5. separate allOrderedItems by category using filter
+     * 6. now get the quantity by using length: pizzas.length
+     * 7. for each category use reduce to get the total amount spent on this category
+     * 
+    */
+     app.get('/order-stats', async (req, res) =>{
+      const pipeline = [
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'orderItemId',
+            foreignField: '_id',
+            as: 'menuItemsData'
+          }
+        },
+        {
+          $unwind: '$menuItemsData'
+        },
+        {
+          $group: {
+            _id: '$menuItemsData.category',
+            count: { $sum: 1 },
+            total: { $sum: '$menuItemsData.price' }
+          }
+        },
+        {
+          $project: {
+            category: '$_id',
+            count: 1,
+            total: { $round: ['$total', 2] },
+            _id: 0
+          }
+        }
+      ];
+
+      const result = await paymentCollection.aggregate(pipeline).toArray()
+      res.send(result)
+
     })
 
     // Send a ping to confirm a successful connection
